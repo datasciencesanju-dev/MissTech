a5ccddf4b4f0713be87711b0a52b9c1d5c26ac9d
Sanju <datasciencesanju@gmail.com>
2026-09-23 15:07:17 +0530

    feat: configure seamless Vercel full-stack deployment (Frontend + Backend API)

--- Frontend/api/Routes/CourseRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllCourses, seedCourses } = require("../Controllers/CourseController");
-
-router.get("/", getAllCourses);
-router.post("/seed", seedCourses);
-
-module.exports = router;
--- Frontend/api/Routes/LoginRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { userLogin } = require("../Controllers/LoginController");
-
-const router = express.Router();
-router.post("/login", userLogin);
-module.exports = router;
